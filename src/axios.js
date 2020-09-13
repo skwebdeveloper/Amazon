2afd88554e535fa3d083d7e79698bde54f31ac03
Saurabh kumar <43346025+skwebdeveloper@users.noreply.github.com>
2020-09-13 11:40:04 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/fir-aa6b6/us-central1/api"
+    baseURL: "http://localh**********************i"
 });
 
 export default instance; 
